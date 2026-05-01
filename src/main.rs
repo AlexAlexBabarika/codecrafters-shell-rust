24a271d4c562f02ec80fabec94e297cf6d6ce2f9
Alex <alexalexbabarika@gmail.com>
2026-05-01 15:12:57 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use std::io::{Write, stdin, stdout};
 
 fn main() {
     let mut input = String::new();
-    print!("$ ");
-    let _ = stdout().flush().unwrap();
-    stdin().read_line(&mut input).unwrap();
-    if let Some('\n') = input.chars().next_back() {
-        input.pop();
-        println!("{}: command not found", input)
+    loop {
+        print!("$ ");
+        let _ = stdout().flush().unwrap();
+        stdin().read_line(&mut input).unwrap();
+        if let Some('\n') = input.chars().next_back() {
+            input.pop();
+            println!("{}: command not found", input);
+            input.clear();
+        }   
+
+        stdout().flush().unwrap();
     }
-    
-    stdout().flush().unwrap();
 }
