722cbdb18ddb251a7064a7a1049299e97957b055
Alex <alexalexbabarika@gmail.com>
2026-05-07 16:49:12 +0100

    codecrafters submit [skip ci]

--- src/util/write_to_file.rs
+++ src/util/write_to_file.rs
@@ -8,7 +8,13 @@ pub fn write_to_file(path: &str, content: &str) -> std::io::Result<()> {
 }
 
 pub fn append_to_file(path: &str, content: &str) -> std::io::Result<()> {
-    let mut file = File::options().append(true).open(path)?;
-    file.write_all(content.as_bytes())?;
-    Ok(())
-}
+        match File::options().append(true).open(path) {
+        Ok(mut file) => {
+            file.write_all(content.as_bytes())?;
+            Ok(())
+        }
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => write_to_file(path, content),
+            _ => Err(e),
+        },
+    }
