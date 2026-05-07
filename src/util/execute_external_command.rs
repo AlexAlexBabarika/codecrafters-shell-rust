5315861666c99a2a26f75dbe18edd61c7b7859a8
Alex <alexalexbabarika@gmail.com>
2026-05-07 14:30:17 +0100

    codecrafters submit [skip ci]

--- src/util/execute_external_command.rs
+++ src/util/execute_external_command.rs
@@ -2,19 +2,13 @@ use crate::commands::shell_command::ShellCommandError;
 use std::process::{Command, Stdio};
 
 pub fn execute_external_command(command: &str, args: &[String]) -> Result<(), ShellCommandError> {
-    let status = Command::new(command)
+    Command::new(command)
         .args(args)
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .status()
         .map_err(|e| ShellCommandError::ExternalCommandError(e.to_string()))?;
-
-    if !status.success() {
-        return Err(ShellCommandError::ExternalCommandError(format!(
-            "process exited with {status}",
-        )));
-    }
 
     Ok(())
 }
@@ -29,13 +23,6 @@ pub fn execute_external_command_capture_stdout(
         .stderr(Stdio::inherit())
         .output()
         .map_err(|e| ShellCommandError::ExternalCommandError(e.to_string()))?;
-
-    if !output.status.success() {
-        return Err(ShellCommandError::ExternalCommandError(format!(
-            "process exited with {}",
-            output.status,
-        )));
-    }
 
     String::from_utf8(output.stdout)
         .map_err(|e| ShellCommandError::ExternalCommandError(e.to_string()))
