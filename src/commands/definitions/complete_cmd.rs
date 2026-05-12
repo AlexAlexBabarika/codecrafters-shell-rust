31b5f01e65ae365586f7709c14f2c3e5e9a365de
Alex <alexalexbabarika@gmail.com>
2026-05-12 15:57:14 +0100

    codecrafters submit [skip ci]

--- src/commands/definitions/complete_cmd.rs
+++ src/commands/definitions/complete_cmd.rs
@@ -3,6 +3,7 @@ use crate::commands::command_props::CommandProps;
 use crate::commands::command_result::CommandResult;
 use crate::commands::shell_command::Execute;
 use crate::commands::shell_command::ShellCommandError;
+use crate::util::check_arguments_length::check_arguments_length;
 
 pub struct COMPLETE {
     #[allow(dead_code)]
@@ -14,16 +15,29 @@ impl COMPLETE {
         COMPLETE {
             props: CommandProps {
                 name: BuiltinCommandName::Complete,
-                max_args: 1,
+                max_args: 2,
             },
         }
     }
 }
 
 impl Execute for COMPLETE {
-    fn execute(&self, _args: &[String]) -> Result<CommandResult, ShellCommandError> {
-        Ok(CommandResult {
-            message: Some("COMPLETE command is not implemented yet".to_string()),
-        })
+    fn execute(&self, args: &[String]) -> Result<CommandResult, ShellCommandError> {
+        check_arguments_length(args, &self.props)?;
+        if args.first().map(|s| s.as_str()) == Some("-p") {
+            if let Some(cmd_name) = args.get(1) {
+                return Ok(CommandResult {
+                    message: Some(
+                        format!(
+                            "{}: {}: no completion specification",
+                            self.props.name, cmd_name
+                        )
+                        .to_string(),
+                    ),
+                });
+            }
+        }
+
+        Ok(CommandResult { message: None })
     }
 }
